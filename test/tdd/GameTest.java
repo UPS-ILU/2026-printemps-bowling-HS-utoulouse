e232ebb83dc2231b4b4b2e0df0e72200c960fb17
hugo <hugo031c@gmail.com>
2026-04-16 17:20:20 +0200

    ITER3

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -28,5 +28,13 @@ class GameTest {
 		}
 		assertEquals(game.score(), 0);
 	}
+	
+	@Test
+	void ITER3() {
+		for (int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertEquals(game.score(), 20);
+	}
 
 }
